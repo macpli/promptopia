8c5d00f8c41afab6ea5814e1fe0e737fb16f1a8a
macpli <pliszekmaciej0@gmail.com>
2024-10-24 05:28:03 +0200

    disable server-side caching

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -2,13 +2,14 @@ import { connectTODB } from "@utils/database";
 import Prompt from "@models/prompt";
 
 // GET
-export const GET = async (request, {params}) => {
+export const GET = async (request,res, {params}) => {
     try {
         await connectTODB();
-
+        
         const prompt = await Prompt.findById(params.id).populate("creator");
         if(!prompt) return new Response("Prompt not found", { status: 404 });
-
+        res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+        res.setHeader('Pragma', 'no-cache');
         return new Response(JSON.stringify(prompt), { status: 200 });
     } catch (error) {
         console.log(error);
